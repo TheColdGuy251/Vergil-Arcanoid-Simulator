b6c26a3f1f6768cd1fbe1d8f46be758c904c892d
VoidKingRus1 <voidkingrus@yandex.ru>
2023-01-14 14:32:09 +0500

    Vergil Movement (bad)

--- Vergil-Arcanoid-Simulator.py
+++ Vergil-Arcanoid-Simulator.py
@@ -1,9 +1,86 @@
+import os
 import pygame
-"Test"
+import sys
+import random
 
-class BaseBox:
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class BaseBox(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = load_image("pt.png")
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        self.rect = self.rect.move(0, 1)
+
+
+class ButtonBox(BaseBox):
     pass
 
 
-class Buttonbox(BaseBox):
-    pass
+class Vergil(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = load_image("vergil.png")
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.key = 0
+
+    def update(self):
+        if self.key == 97:
+            self.rect.x -= 10
+        elif self.key == 100:
+            self.rect.x += 10
+
+    def move(self, key):
+        self.key = key
+
+
+if __name__ == "__main__":
+    pygame.init()
+    width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Vergil Arcanoid Simulator")
+    all_sprites = pygame.sprite.Group()
+    vergil = Vergil(((width / 2 ) - 100, height - 200))
+    horizontal_borders = pygame.sprite.Group()
+    vertical_borders = pygame.sprite.Group()
+    fps = 50
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                vergil.move(event.key)
+            if event.type == pygame.KEYUP:
+                vergil.move(0)
+        BaseBox((random.randint(10, width - 0.2 * width), -20))
+        screen.fill((7, 0, 36))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        pygame.display.flip()
+        clock.tick(fps)
+    pygame.quit()
