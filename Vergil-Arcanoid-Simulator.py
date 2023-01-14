d30b8915a3f61aeaabe1f48058c8ddf264156c12
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-01-14 12:40:01 +0500

    window and fullscreen creation

--- Vergil-Arcanoid-Simulator.py
+++ Vergil-Arcanoid-Simulator.py
@@ -1,5 +1,5 @@
 import pygame
-"Test"
+
 
 class BaseBox:
     pass
@@ -7,3 +7,17 @@ class BaseBox:
 
 class Buttonbox(BaseBox):
     pass
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Vergil Arcanoid Simulator')
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    running = True
+    drawing = False
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
+    pygame.quit()
