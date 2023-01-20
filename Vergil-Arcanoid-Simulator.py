664718404a3a34a4eeb18a21f2f39fd2a849772c
Ilya <smirnovilya7031@gmail.com>
2023-01-20 17:37:35 +0500

    music and sound ideas prototype (almost empty functions)

--- Vergil-Arcanoid-Simulator.py
+++ Vergil-Arcanoid-Simulator.py
@@ -1,9 +1,148 @@
+import os
 import pygame
-"Test"
+import sys
+import random
 
-class BaseBox:
+
+pygame.mixer.pre_init(44100, -16, 1, 512)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class BaseBox(pygame.sprite.Sprite):
     pass
 
 
-class Buttonbox(BaseBox):
+class ButtonBox(BaseBox):
     pass
+
+
+class Vergil(pygame.sprite.Sprite):
+    pass
+
+
+def main_menu_music_player():
+    pygame.mixer.Music.play("data/music/main_menu.ogg")
+
+
+def music_player(style_rank):
+    if style_rank == 0:
+        pygame.mixer.Sound.play("data/music/bury_the_light_intro.ogg")
+    elif style_rank == 1:
+        pygame.mixer.Sound.play("data/music/bury_the_light_no_rank.ogg")
+    elif style_rank == 2:
+        pygame.mixer.Sound.play("data/music/bury_the_light_dismal.ogg")
+    elif style_rank == 3:
+        pygame.mixer.Sound.play("data/music/bury_the_light_crazy.ogg")
+    elif style_rank == 4:
+        pygame.mixer.Sound.play("data/music/bury_the_light_badass.ogg")
+    elif style_rank == 5:
+        pygame.mixer.Sound.play("data/music/bury_the_light_apocalyptic.ogg")
+    elif style_rank == 6:
+        pygame.mixer.Sound.play("data/music/bury_the_light_savage.ogg")
+    elif style_rank == 7:
+        pygame.mixer.Sound.play("data/music/bury_the_light_sick_skills.ogg")
+    elif style_rank == 8:
+        pygame.mixer.Sound.play("data/music/bury_the_light_smokin_sexy_style.ogg")
+
+
+def judgement_cut(times):
+    pygame.mixer.Sound.play("data/sound/judgement_cut.ogg")
+    if times == 3:
+        pygame.mixer.Sound.play("data/dialogues/jackpot.ogg")
+
+
+def doppleganger():
+    pygame.mixer.Sound.play("data/sound/doppleganger_spawn.ogg")
+
+
+def judgement_cut_end(style_rank):
+    if style_rank == 5:
+        pass
+    else:
+        pygame.mixer.Sound.play("data/sound/judgement_cut_end_main.ogg")
+        if random.randint(0, 1) == 0:
+            pygame.mixer.Sound.play("data/dialogues/slay_all.ogg")
+        else:
+            pygame.mixer.Sound.play("data/dialogues/you_shall_die.ogg")
+
+
+def sin_devil_trigger():
+    pygame.mixer.Sound.play("data/sound/sdt_transformation.ogg")
+    if random.randint(0, 1) == 0:
+        pygame.mixer.Sound.play("data/dialogues/nightmare_begins.ogg")
+    else:
+        pygame.mixer.Sound.play("data/dialogues/this_is_power.ogg")
+
+
+def damage_taken(interval):
+    a = random.randint(0, 2)
+    if a == 0:
+        pygame.mixer.Music.play("data/sound/hurt1.ogg")
+    elif a == 1:
+        pygame.mixer.Music.play("data/sound/hurt2.ogg")
+    else:
+        pygame.mixer.Music.play("data/sound/hurt3.ogg")
+
+    if interval >= 5000:
+        a = random.randint(0, 2)
+        if a == 0:
+            pygame.mixer.Music.play("data/dialogue/hurt1.ogg")
+        elif a == 1:
+            pygame.mixer.Music.play("data/dialogue/hurt2.ogg")
+        else:
+            pygame.mixer.Music.play("data/dialogue/hurt3.ogg")
+
+
+def random_dialogues(interval):
+    if interval >= 10000:
+        a = random.randint(0, 5)
+        if a == 0:
+            pygame.mixer.Music.play("data/dialogue/show_me_your_motivation.ogg")
+        elif a == 1:
+            pygame.mixer.Music.play("data/dialogue/you_not_worthy_as_my_opponent.ogg")
+        elif a == 2:
+            pygame.mixer.Music.play("data/dialogue/scum.ogg")
+        elif a == 3:
+            pygame.mixer.Music.play("data/dialogue/how_boring.ogg")
+        elif a == 4:
+            pygame.mixer.Music.play("data/dialogue/your_wasting_my_time.ogg")
+        elif a == 5:
+            pygame.mixer.Music.play("data/dialogue/now_im_a_little_motivated.ogg")
+
+
+if __name__ == "__main__":
+    pygame.init()
+    width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Vergil Arcanoid Simulator")
+    all_sprites = pygame.sprite.Group()
+    horizontal_borders = pygame.sprite.Group()
+    vertical_borders = pygame.sprite.Group()
+    fps = 50
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((7, 0, 36))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        pygame.display.flip()
+        clock.tick(fps)
+    pygame.quit()
