2a64f8fe928cbe6e13604ee68ae8579d61255a85
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-01-24 21:56:04 +0500

    final variant of main menu

--- Vergil-Arcanoid-Simulator.py
+++ Vergil-Arcanoid-Simulator.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+pygame.mixer.init()
+
 
 class Button():
     def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
@@ -10,17 +12,11 @@ class Button():
         self.height = height
         self.onclickFunction = onclickFunction
         self.onePress = onePress
-
-        self.fillColors = {
-            'normal': '#000000',
-            'hover': '#009999',
-            'pressed': '#333333',
-        }
+        self.buttonText = buttonText
+        self.flicked = False
 
         self.buttonSurface = pygame.Surface((self.width, self.height))
         self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
-
-        self.buttonSurf = font.render(buttonText, True, (255, 255, 255))
 
         self.alreadyPressed = False
 
@@ -31,13 +27,22 @@ class Button():
         self.buttonSurface = self.buttonSurface.convert_alpha()
         if not self.buttonRect.collidepoint(mousePos):
             self.buttonSurface.fill((0, 0, 0, 0))
+            font = pygame.font.Font('data/font/DMC5Font.otf', 40)
+            self.buttonSurf = font.render(self.buttonText, True, (255, 255, 255))
+            self.flicked = False
         if self.buttonRect.collidepoint(mousePos):
-            self.buttonSurface.fill((0, 0, 255, 0))
-            shape_surf = pygame.Surface(pygame.Rect(0, self.y, 1000, self.height).size, pygame.SRCALPHA)
-            pygame.draw.rect(shape_surf, (0, 0, 255, 120), shape_surf.get_rect())
-            screen.blit(shape_surf, (0, self.y, 1000, self.height))
+            self.buttonSurface.fill((0, 39, 62, 0))
+            shape_surf = pygame.Surface(pygame.Rect(0, self.y, pygame.display.Info().current_w, self.height).size,
+                                        pygame.SRCALPHA)
+            pygame.draw.rect(shape_surf, (0, 39, 62, 230), shape_surf.get_rect())
+            screen.blit(shape_surf, (0, self.y, pygame.display.Info().current_w, self.height))
+            font = pygame.font.Font('data/font/DMC5Font.otf', 55)
+            if not self.flicked:
+                pygame.mixer.Sound('data/sounds/flicking sound.ogg').play()
+                self.flicked = True
+            self.buttonSurf = font.render(self.buttonText, True, (124, 255, 255))
             if pygame.mouse.get_pressed(num_buttons=3)[0]:
-                self.buttonSurface.fill(self.fillColors['pressed'])
+                pygame.mixer.Sound('data/sounds/enter sound.ogg').play()
 
                 if self.onePress:
                     self.onclickFunction()
@@ -59,28 +64,31 @@ class Button():
 def myFunction():
     print('Button Pressed')
 
+def main_menu_music():
+    pygame.mixer.music.load("data/music/Wake your fury and wait in hell.ogg")
+    pygame.mixer.music.set_volume(0.3)
+    pygame.mixer.music.play(-1)
+
 
 pygame.init()
 fps = 60
 fpsClock = pygame.time.Clock()
-width, height = 640, 480
-screen = pygame.display.set_mode((width, height))
-
-font = pygame.font.SysFont('Arial', 40)
+width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 objects = []
-transparent = 0
-customButton = Button(40, 30, 400, 50, 'Начать игру', myFunction)
-customButton = Button(30, 140, 400, 50, 'Обучение', myFunction)
-customButton = Button(30, 200, 400, 50, 'Выйти', myFunction)
+customButton = Button(width / 2 - 200, height * 0.65, 400, 60, 'Begin', myFunction)
+customButton = Button(width / 2 - 200, height * 0.78, 400, 60, 'Tutorial', myFunction)
+customButton = Button(width / 2 - 200, height * 0.85, 400, 60, 'Exit', myFunction)
+bg = pygame.image.load("data/vergil background.png")
+main_menu_music()
 running = True
 while running:
-    screen.fill((20, 20, 20))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+    screen.blit(bg, (0, 0))
     for object in objects:
         object.process()
 
